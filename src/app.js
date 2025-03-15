a0fcc7498c90ea765a9ef2255a24c150e2d1bbaf
Gowthami <gowthamisammiti@gmail.com>
2025-03-15 17:54:16 +0530

    Used HTTP methods

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 
 const app = express();
+app.get("/user", (req, res) => {
+  console.log("Hey this is Get method for user");
+  res.send("Get Success");
+});
 
+app.post("/user", (req, res) => {
+  console.log("Hey this is POST method for user");
+  res.send("POST Success");
+});
+
+app.delete("/user", (req, res) => {
+  console.log("Hey this is delete method for user");
+  res.send("DELETE Success");
+});
 app.use((req, res) => {
   res.send("Hello port 3000!!!!!!!hi");
 });
