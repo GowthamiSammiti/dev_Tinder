4af5b77a442d4dce52b583858355e69cabf89f7b
Gowthami <gowthamisammiti@gmail.com>
2025-03-18 18:55:23 +0530

    Get and post Users

--- src/app.js
+++ src/app.js
@@ -5,18 +5,40 @@ const app = express();
 const connectDb = require("./config/dataBase");
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Nadhu",
-    lastName: "S",
-    emailId: "Nandhu@gmail.com",
-    password: "NS@123",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("user added !!!!!!!!!");
   } catch (error) {
     res.status(500).send("Error with adding user" + error.message);
+  }
+});
+
+app.get("/getUser", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId });
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(400).send("User not found!!! ");
+    }
+  } catch (error) {
+    res.status(400).send("Spmething is wrong!!!");
+  }
+});
+app.get("/getAllUsers", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users) {
+      res.send(users);
+    } else {
+      res.status(400).send("User not found!!! ");
+    }
+  } catch (error) {
+    res.status(400).send("Spmething is wrong!!!");
   }
 });
 
